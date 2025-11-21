190137825b5923a7d2723b7ebe386818e4e700e2
vitorvhsilva <dndragonbr@gmail.com>
2025-11-21 15:15:16 -0300

    cors config

--- GlobalSolutionJava/GlobalSolutionJava/src/main/java/br/com/fiap/GlobalSolutionJava/config/security/SecurityConfig.java
+++ GlobalSolutionJava/GlobalSolutionJava/src/main/java/br/com/fiap/GlobalSolutionJava/config/security/SecurityConfig.java
@@ -46,10 +46,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8081")); // front-end
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
+        configuration.setAllowCredentials(false);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
